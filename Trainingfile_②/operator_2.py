e9cace59185c204d853f546ecb618ea6884b86b0
Shohei <124876173+Shohei1129@users.noreply.github.com>
2023-06-08 20:17:00 +0900

    Add files via upload

---
+++ Trainingfile_②/operator_2.py
@@ -0,0 +1,8 @@
+#文字列結合
+a = "aaa"
+b = "bbb"
+print(a+b)
+
+#文字列の中からの抽出
+a = "abcdefhig"
+print(a[5])
---
+++ Trainingfile_②/print_3.py
@@ -0,0 +1,10 @@
+#print１行で表示
+print("a,b,c")
+
+#print分けによる複数行の表示
+print("a")
+print("b")
+print("c")
+
+#[\n]による改行表示
+print("a\nb\nc\n")
